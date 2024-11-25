f7c32c9a500886669ab1d336737cce5b08f05ce7
RyanRanjith <kingranjith4@gmail.com>
2024-11-25 10:28:26 -0800

    A

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,13 @@
+
+
+
+// APu for adding doctor
+
+const addDoctor = async(req,res) => {
+
+    try {
+        const {name,email,password,speciality,degree,experience,about,fees,address} = req.body
+    } catch (error) {
+        
+    }
+}
