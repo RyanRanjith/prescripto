5802a615adb9b6785521f9c9d6fe4d42f803f8fc
RyanRanjith <kingranjith4@gmail.com>
2024-12-13 08:45:01 -0800

    a

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {AppContext} from '../context/AppContext'
 import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const MyAppointments = () => {
 
@@ -13,12 +14,23 @@ const getUserAppointments = async () => {
     const {data} = await axios.get(backendUrl+'/api/user/appointments',{headers:{token}})
 
     if (data.success) {
-      setAppointments(data.appointments)
+      setAppointments(data.appointments.reverse())
+      console.log(data.appointments);
+      
+
     }
   } catch (error) {
+    console.log(error);
+    toast.error(error.message)
     
   }
 }
+
+useEffect(()=>{
+   if (token) {
+    getUserAppointments()
+   }
+},[token])
 
   return (
     <div>
@@ -27,15 +39,15 @@ const getUserAppointments = async () => {
         {appointments.map((item,index)=>(
           <div className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b" key={index}>
               <div className="">
-                <img className='w-32 bg-indigo-50 rounded-md' src={item.image} alt="" />
+                <img className='w-32 bg-indigo-50 rounded-md' src={item.docData.image} alt="" />
               </div>
               <div className="flex-1 text-sm text-zinc-600">
-                <p className='text-neutral-800 font-semibold'>{item.name}</p>
-                <p>{item.speciality}</p>
+                <p className='text-neutral-800 font-semibold'>{item.docData.name}</p>
+                <p>{item.docData.speciality}</p>
                 <p className='text-zinc-700 font-medium mt-1'>Address:</p>
-                <p className='text-xs'>{item.address.line1}</p>
-                <p className='text-xs'>{item.address.line2}</p>
-                <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time: </span> 25, July, 2024 |  8:30 PM</p>
+                <p className='text-xs'>{item.docData.address.line1}</p>
+                <p className='text-xs'>{item.docData.address.line2}</p>
+                <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time: </span> {item.slotDate} |  {item.slotTime}</p>
               </div>
               <div className=""></div>
               <div className="flex flex-col gap-2 justify-end">
