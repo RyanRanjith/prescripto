14d75aa53d87e12de7dfb7a304f8f791ce6a6f5e
RyanRanjith <kingranjith4@gmail.com>
2025-01-06 07:24:01 -0800

    a

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -11,6 +11,7 @@ const DoctorContextProvider = (props) => {
   const [dToken, setDToken] = useState(localStorage.getItem('dToken')?localStorage.getItem('dToken'):'')
   const [appointments, setAppointments] = useState([])
   const [dashData, setDashData] = useState(false)
+  const [profileData, setProfileData] = useState(false)
 
   const getAppointments = async () => {
     try {
@@ -26,6 +27,20 @@ const DoctorContextProvider = (props) => {
       console.log(error);
       toast.error(error.message)
       
+    }
+  }
+
+  const getProfileData = async () => {
+    try {
+      const {data} = await axios.get(backendUrl + '/api/doctor/profile', {headers:{dToken}})
+      if (data.success) {
+        setProfileData(data.profileData) 
+        console.log(data.profileData);
+        
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
     }
   }
 
@@ -81,7 +96,7 @@ const getDashData = async () => {
 
 
 const value = {
-  dToken,setDToken,backendUrl,appointments,setAppointments,getAppointments,completeAppointment,cancelAppointment,dashData, setDashData, getDashData
+  dToken,setDToken,backendUrl,appointments,setAppointments,getAppointments,completeAppointment,cancelAppointment,dashData, setDashData, getDashData, profileData, setProfileData, getProfileData,
 }
 
 return (
--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { DoctorContext } from '../../context/DoctorContext'
+import { AppContext } from '../../context/AppContext'
 
 const DoctorProfile = () => {
-  return (
+
+  const {dToken, profileData, setProfileData, getProfileData} = useContext(DoctorContext)
+  const {currency, backendUrl} = useContext(AppContext)
+
+  useEffect(()=>{
+    if (dToken) {
+      getProfileData()
+    }
+  },[dToken])
+
+  return profileData && (
     <div>
-      
+      <div className=" flex flex-col gap-4 m-5">
+        <div className="">
+          <img src={profileData.image} className='bg-primary/80 w-full sm:max-w-64 rounded-lg' alt="" />
+        </div> 
+        <div className="flex-1 border border-stone-100 rounded-lg p-8 py-7 bg-white ">
+          {/* Doc info : name , degree and experience */} 
+          <p className='flex items-center gap-2 text-3xl font-medium text-gray-700'>{profileData.name}</p>
+          <div className="flex items-center gap-2 mt-1 text-gray-600">
+            <p>{profileData.degree} - {profileData.speciality}</p> 
+            <button className='py-0.5 px-2 border text-xs rounded-full'>{profileData.experience} Years </button>
+          </div> 
+          {/* Doc About  */} 
+          <div className="">
+             <p className='flex items-center gap-1 text-sm font-medium text-neutral-800 mt-3'>About:</p> 
+             <p>
+              {profileData.about}
+             </p>
+          </div>
+          <p>
+            Appointment fee: <span>{currency} {profileData.fees}</span>
+          </p> 
+          <div className="">
+            <p>Address:</p> 
+            <p>
+              {profileData.address.line1}
+              <br />
+              {profileData.address.line2}
+            </p>
+          </div> 
+
+          <div className="">
+            <input type="checkbox" name="id" id="" /> 
+            <label htmlFor="">Available</label>
+          </div> 
+
+          <button>Edit</button> 
+
+        </div>
+      </div>
     </div>
   )
 }
