52febabbf8cc4783451e0d96e9e0ab5bde039395
RyanRanjith <kingranjith4@gmail.com>
2025-01-07 07:30:00 -0800

    a

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -1,11 +1,13 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { DoctorContext } from '../../context/DoctorContext'
 import { AppContext } from '../../context/AppContext'
 
 const DoctorProfile = () => {
 
   const {dToken, profileData, setProfileData, getProfileData} = useContext(DoctorContext)
-  const {currency, backendUrl} = useContext(AppContext)
+  const {currency, backendUrl} = useContext(AppContext) 
+
+  const [isEdit, setIsEdit] = useState(false)
 
   useEffect(()=>{
     if (dToken) {
@@ -29,28 +31,32 @@ const DoctorProfile = () => {
           {/* Doc About  */} 
           <div className="">
              <p className='flex items-center gap-1 text-sm font-medium text-neutral-800 mt-3'>About:</p> 
-             <p>
+             <p className='text-sm text-gray-600 max-w-[700px] mt-1'>
               {profileData.about}
              </p>
           </div>
-          <p>
-            Appointment fee: <span>{currency} {profileData.fees}</span>
+          <p className='text-gray-600 font-medium mt-4'>
+            Appointment fee: <span className='text-gray-800'>{currency} {isEdit ? <input type="number" onChange={(e)=>setProfileData(prev => ({...prev, fees:e.target.value}))} value={profileData.fees} /> : profileData.fees}</span>
           </p> 
-          <div className="">
+          <div className="flex gap-2 py-2">
             <p>Address:</p> 
-            <p>
-              {profileData.address.line1}
+            <p className='text-sm'>
+              {isEdit ? <input type="text" onChange={(e)=>setProfileData(prev => ({...prev, address:{...prev.address,line1:e.target.value}}))} value={profileData.address.line1} /> : profileData.address.line1}
               <br />
-              {profileData.address.line2}
+              {isEdit ? <input type="text" onChange={(e)=>setProfileData(prev => ({...prev, address:{...prev.address,line2:e.target.value}}))} value={profileData.address.line2} /> : profileData.address.line2}
             </p>
           </div> 
 
-          <div className="">
-            <input type="checkbox" name="id" id="" /> 
+          <div className="flex gap-1 pt-2">
+            <input onChange={()=> isEdit && setProfileData(prev => ({...prev, available: !prev.available}))} checked={profileData.available} type="checkbox" name="id" id="" /> 
             <label htmlFor="">Available</label>
           </div> 
 
-          <button>Edit</button> 
+          {
+            isEdit 
+            ?   <button onClick={()=>setIsEdit(false)} className='px-4 py-1 border border-primary text-sm rounded-full mt-5 hover:bg-primary hover:text-white transition-all '>Save</button> 
+            : <button onClick={()=>setIsEdit(true)} className='px-4 py-1 border border-primary text-sm rounded-full mt-5 hover:bg-primary hover:text-white transition-all '>Edit</button> 
+          }
 
         </div>
       </div>
