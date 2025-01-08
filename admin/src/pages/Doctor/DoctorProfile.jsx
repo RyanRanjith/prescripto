565c51db3ed2ce4339e1d0669dd6d8df1f2e5c7b
RyanRanjith <kingranjith4@gmail.com>
2025-01-08 07:30:03 -0800

    a

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -1,13 +1,39 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { DoctorContext } from '../../context/DoctorContext'
 import { AppContext } from '../../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const DoctorProfile = () => {
 
-  const {dToken, profileData, setProfileData, getProfileData} = useContext(DoctorContext)
-  const {currency, backendUrl} = useContext(AppContext) 
+  const {dToken, profileData, setProfileData, getProfileData, backendUrl} = useContext(DoctorContext)
+  const {currency, } = useContext(AppContext) 
 
-  const [isEdit, setIsEdit] = useState(false)
+  const [isEdit, setIsEdit] = useState(false) 
+
+  const updateProfile = async () => {
+    try {
+      const updateData = {
+        address: profileData.address,
+        fees: profileData.fees,
+        available: profileData.available
+      } 
+
+      const {data} = await axios.post(backendUrl + '/api/doctor/update-profile', updateData,{headers:{dToken}}) 
+      if (data.success) {
+        toast.success(data.message) 
+        setIsEdit(false) 
+        getProfileData()
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message) 
+      console.log(error);
+      
+    } 
+  }
+
 
   useEffect(()=>{
     if (dToken) {
@@ -54,7 +80,7 @@ const DoctorProfile = () => {
 
           {
             isEdit 
-            ?   <button onClick={()=>setIsEdit(false)} className='px-4 py-1 border border-primary text-sm rounded-full mt-5 hover:bg-primary hover:text-white transition-all '>Save</button> 
+            ?   <button onClick={updateProfile} className='px-4 py-1 border border-primary text-sm rounded-full mt-5 hover:bg-primary hover:text-white transition-all '>Save</button> 
             : <button onClick={()=>setIsEdit(true)} className='px-4 py-1 border border-primary text-sm rounded-full mt-5 hover:bg-primary hover:text-white transition-all '>Edit</button> 
           }
 
